859495d87e8fa96cfd16b1e5a636f39e741c7e5a
Hans-Joachim Krauch <achim-k@users.noreply.github.com>
2021-03-08 13:36:30 -0300

    Change copyright year 2020 -> 2021.

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Pickit'
-copyright = u'2020, Pickit N.V.'
+copyright = u'2021, Pickit N.V.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
